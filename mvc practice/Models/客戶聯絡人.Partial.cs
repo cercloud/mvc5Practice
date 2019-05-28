9e7ae18684ca57b455a30758eec3ce702dcc4165
簡洋志 <chienyc00@gmail.com>
2019-05-28 14:33:23 +0800

    第一周

---
+++ mvc practice/Models/客戶聯絡人.Partial.cs
@@ -0,0 +1,69 @@
+namespace mvc_practice.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(客戶聯絡人MetaData))]
+    public partial class 客戶聯絡人 : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var db = new CustomersEntities();
+
+            if (this.Id == 0)
+            {
+                //Create
+                if (db.客戶聯絡人.Where(p => p.客戶Id == this.客戶Id && p.Email == this.Email).Any())
+                {
+                    yield return new ValidationResult("已存在相同Email", new string[] { "Email" });
+                }
+            }
+            else
+            {
+                //Update
+                if (db.客戶聯絡人.Where(p => p.Id != this.Id && p.Email == this.Email).Any())
+                {
+                    yield return new ValidationResult("已存在相同Email", new string[] { "Email" });
+                }
+            }
+
+            yield return ValidationResult.Success;
+        }
+    }
+
+    public partial class 客戶聯絡人MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int 客戶Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 職稱 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 姓名 { get; set; }
+        
+        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [RegularExpression(@"\d{4}-\d{5}", ErrorMessage = "必須為09XX-XXXXX")]
+        public string 手機 { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string 電話 { get; set; }
+        [Required]
+        public bool 刪除 { get; set; }
+    
+        public virtual 客戶資料 客戶資料 { get; set; }
+    }
+}
